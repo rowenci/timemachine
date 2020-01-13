dc8561572443b8f6819a6523888fddc1074b6048
rowenci <459287499@qq.com>
2020-01-13 11:43:15 +0800

    no message

--- src/main/java/com/rowenci/timemachine/TimemachineApplication.java
+++ src/main/java/com/rowenci/timemachine/TimemachineApplication.java
@@ -1,9 +1,11 @@
 package com.rowenci.timemachine;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.rowenci.timemachine.mapper")
 public class TimemachineApplication {
 
     public static void main(String[] args) {
