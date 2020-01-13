9f05ba071eaf6c5dbb3ca86ea1dddc713c3cfc9c
rowenci <459287499@qq.com>
2020-01-13 09:59:07 +0800

    no message

---
+++ src/main/java/com/rowenci/timemachine/TimemachineApplication.java
@@ -0,0 +1,13 @@
+package com.rowenci.timemachine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimemachineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimemachineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rowenci/timemachine/TimemachineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rowenci.timemachine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimemachineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
